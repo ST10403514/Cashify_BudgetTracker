1d8adceb3a42c731d18ee6ddcde2ac584b652fc2
Matthew Mason <st10403514@vcconnect.edu.za>
2025-05-02 12:05:05 +0200

    Updated Db Version

--- app/src/main/java/com/mason/cashify_budgettracker/data/AppDatabase.kt
+++ app/src/main/java/com/mason/cashify_budgettracker/data/AppDatabase.kt
@@ -6,7 +6,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Category::class, Expense::class, Goal::class, User::class], version = 7, exportSchema = false)
+@Database(entities = [Category::class, Expense::class, Goal::class, User::class], version = 9, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun expenseDao(): ExpenseDao
